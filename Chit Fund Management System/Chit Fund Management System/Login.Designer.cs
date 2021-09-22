823605db8604121099f2300f7b128ef1fb01033d
RonitK <ronitkohinkar001@gmail.com>
2021-09-22 19:16:03 +0530

    CompanyProfile

--- Chit Fund Management System/Chit Fund Management System/Login.Designer.cs
+++ Chit Fund Management System/Chit Fund Management System/Login.Designer.cs
@@ -85,7 +85,7 @@ namespace Chit_Fund_Management_System
             // 
             // bt_clearlogin
             // 
-            this.bt_clearlogin.BackColor = System.Drawing.Color.Gray;
+            this.bt_clearlogin.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.bt_clearlogin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_clearlogin.Location = new System.Drawing.Point(390, 266);
             this.bt_clearlogin.Name = "bt_clearlogin";
@@ -96,7 +96,7 @@ namespace Chit_Fund_Management_System
             // 
             // bt_login
             // 
-            this.bt_login.BackColor = System.Drawing.Color.Gray;
+            this.bt_login.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.bt_login.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_login.Location = new System.Drawing.Point(53, 266);
             this.bt_login.Name = "bt_login";
@@ -112,7 +112,7 @@ namespace Chit_Fund_Management_System
             this.linklb_newuser.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linklb_newuser.Location = new System.Drawing.Point(246, 383);
             this.linklb_newuser.Name = "linklb_newuser";
-            this.linklb_newuser.Size = new System.Drawing.Size(100, 24);
+            this.linklb_newuser.Size = new System.Drawing.Size(121, 29);
             this.linklb_newuser.TabIndex = 12;
             this.linklb_newuser.TabStop = true;
             this.linklb_newuser.Text = "New User";
@@ -125,7 +125,7 @@ namespace Chit_Fund_Management_System
             this.label2.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(45, 183);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(222, 46);
+            this.label2.Size = new System.Drawing.Size(270, 56);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password:";
             // 
@@ -135,7 +135,7 @@ namespace Chit_Fund_Management_System
             this.label4.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(45, 67);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(243, 46);
+            this.label4.Size = new System.Drawing.Size(294, 56);
             this.label4.TabIndex = 0;
             this.label4.Text = "User Name:";
             // 
@@ -146,7 +146,7 @@ namespace Chit_Fund_Management_System
             this.label1.Font = new System.Drawing.Font("Arial", 28F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(282, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(726, 55);
+            this.label1.Size = new System.Drawing.Size(861, 65);
             this.label1.TabIndex = 1;
             this.label1.Text = "Chit Fund Management System";
             // 
@@ -161,7 +161,7 @@ namespace Chit_Fund_Management_System
             // 
             // f_login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
