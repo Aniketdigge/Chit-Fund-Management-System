dcbbe91a42e8359cdad3903ab630c5ab5b070486
Aniket Digge <kccaniketd2016@gmail.com>
2021-10-27 09:49:01 +0530

    Added documentation and pdf

--- Chit Fund Management System/Chit Fund Management System/Login.Designer.cs
+++ Chit Fund Management System/Chit Fund Management System/Login.Designer.cs
@@ -31,6 +31,7 @@ namespace Chit_Fund_Management_System
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(f_login));
             this.logingb = new System.Windows.Forms.GroupBox();
+            this.lb_user_type = new System.Windows.Forms.Label();
             this.tb_passwordlogin = new System.Windows.Forms.TextBox();
             this.tb_usernamelogin = new System.Windows.Forms.TextBox();
             this.bt_clearlogin = new System.Windows.Forms.Button();
@@ -38,17 +39,28 @@ namespace Chit_Fund_Management_System
             this.linklb_newuser = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.lb_user_type = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.logingb.SuspendLayout();
-            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // logingb
             // 
-            this.logingb.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.logingb.BackColor = System.Drawing.Color.LightGray;
             this.logingb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.logingb.Controls.Add(this.button3);
+            this.logingb.Controls.Add(this.button2);
+            this.logingb.Controls.Add(this.pictureBox2);
+            this.logingb.Controls.Add(this.pictureBox1);
             this.logingb.Controls.Add(this.lb_user_type);
             this.logingb.Controls.Add(this.tb_passwordlogin);
             this.logingb.Controls.Add(this.tb_usernamelogin);
@@ -59,36 +71,47 @@ namespace Chit_Fund_Management_System
             this.logingb.Controls.Add(this.label4);
             this.logingb.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.logingb.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.logingb.Location = new System.Drawing.Point(498, 202);
+            this.logingb.Location = new System.Drawing.Point(228, 71);
             this.logingb.Name = "logingb";
-            this.logingb.Size = new System.Drawing.Size(517, 370);
+            this.logingb.Size = new System.Drawing.Size(574, 432);
             this.logingb.TabIndex = 0;
             this.logingb.TabStop = false;
             // 
+            // lb_user_type
+            // 
+            this.lb_user_type.AutoSize = true;
+            this.lb_user_type.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_user_type.Location = new System.Drawing.Point(217, 12);
+            this.lb_user_type.Name = "lb_user_type";
+            this.lb_user_type.Size = new System.Drawing.Size(100, 29);
+            this.lb_user_type.TabIndex = 17;
+            this.lb_user_type.Text = "Welcome";
+            // 
             // tb_passwordlogin
             // 
+            this.tb_passwordlogin.BackColor = System.Drawing.Color.White;
             this.tb_passwordlogin.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_passwordlogin.Location = new System.Drawing.Point(268, 170);
+            this.tb_passwordlogin.Location = new System.Drawing.Point(323, 159);
             this.tb_passwordlogin.Multiline = true;
             this.tb_passwordlogin.Name = "tb_passwordlogin";
             this.tb_passwordlogin.PasswordChar = '*';
-            this.tb_passwordlogin.Size = new System.Drawing.Size(196, 35);
+            this.tb_passwordlogin.Size = new System.Drawing.Size(213, 46);
             this.tb_passwordlogin.TabIndex = 16;
             // 
             // tb_usernamelogin
             // 
             this.tb_usernamelogin.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_usernamelogin.Location = new System.Drawing.Point(268, 76);
+            this.tb_usernamelogin.Location = new System.Drawing.Point(323, 64);
             this.tb_usernamelogin.Multiline = true;
             this.tb_usernamelogin.Name = "tb_usernamelogin";
-            this.tb_usernamelogin.Size = new System.Drawing.Size(196, 31);
+            this.tb_usernamelogin.Size = new System.Drawing.Size(213, 46);
             this.tb_usernamelogin.TabIndex = 15;
             // 
             // bt_clearlogin
             // 
             this.bt_clearlogin.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.bt_clearlogin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_clearlogin.Location = new System.Drawing.Point(268, 242);
+            this.bt_clearlogin.Location = new System.Drawing.Point(356, 302);
             this.bt_clearlogin.Name = "bt_clearlogin";
             this.bt_clearlogin.Size = new System.Drawing.Size(139, 49);
             this.bt_clearlogin.TabIndex = 14;
@@ -99,7 +122,7 @@ namespace Chit_Fund_Management_System
             // 
             this.bt_login.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.bt_login.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_login.Location = new System.Drawing.Point(90, 242);
+            this.bt_login.Location = new System.Drawing.Point(104, 302);
             this.bt_login.Name = "bt_login";
             this.bt_login.Size = new System.Drawing.Size(139, 49);
             this.bt_login.TabIndex = 13;
@@ -111,7 +134,7 @@ namespace Chit_Fund_Management_System
             // 
             this.linklb_newuser.AutoSize = true;
             this.linklb_newuser.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linklb_newuser.Location = new System.Drawing.Point(213, 319);
+            this.linklb_newuser.Location = new System.Drawing.Point(250, 375);
             this.linklb_newuser.Name = "linklb_newuser";
             this.linklb_newuser.Size = new System.Drawing.Size(85, 19);
             this.linklb_newuser.TabIndex = 12;
@@ -124,7 +147,7 @@ namespace Chit_Fund_Management_System
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(45, 159);
+            this.label2.Location = new System.Drawing.Point(106, 159);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(195, 46);
             this.label2.TabIndex = 1;
@@ -134,62 +157,108 @@ namespace Chit_Fund_Management_System
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(45, 64);
+            this.label4.Location = new System.Drawing.Point(106, 64);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(211, 46);
             this.label4.TabIndex = 0;
             this.label4.Text = "User Name :";
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Arial", 28F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(402, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(726, 55);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Chit Fund Management System";
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(48, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 46);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
             // 
-            // panel1
+            // pictureBox2
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1474, 84);
-            this.panel1.TabIndex = 2;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(48, 159);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(52, 46);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
             // 
-            // lb_user_type
+            // pictureBox3
             // 
-            this.lb_user_type.AutoSize = true;
-            this.lb_user_type.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_user_type.Location = new System.Drawing.Point(198, 14);
-            this.lb_user_type.Name = "lb_user_type";
-            this.lb_user_type.Size = new System.Drawing.Size(100, 29);
-            this.lb_user_type.TabIndex = 17;
-            this.lb_user_type.Text = "Welcome";
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 71);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(199, 205);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(839, 46);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(789, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 45);
+            this.button1.TabIndex = 20;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(479, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 41);
+            this.button2.TabIndex = 20;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(417, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 41);
+            this.button3.TabIndex = 21;
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // f_login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackColor = System.Drawing.Color.Gray;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1498, 689);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(839, 546);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.logingb);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "f_login";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.LoginF_Load);
             this.logingb.ResumeLayout(false);
             this.logingb.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -197,7 +266,6 @@ namespace Chit_Fund_Management_System
         #endregion
 
         private System.Windows.Forms.GroupBox logingb;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.LinkLabel linklb_newuser;
@@ -205,8 +273,14 @@ namespace Chit_Fund_Management_System
         private System.Windows.Forms.Button bt_clearlogin;
         private System.Windows.Forms.TextBox tb_passwordlogin;
         private System.Windows.Forms.TextBox tb_usernamelogin;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lb_user_type;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
